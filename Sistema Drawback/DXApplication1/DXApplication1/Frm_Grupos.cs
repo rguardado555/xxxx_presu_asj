214d98cac8fbd167472dd861d6e83c31d848b44a
rguardado555 <rguardado.rsoft@gmail.com>
2018-04-04 09:13:04 -0500

    form

---
+++ Sistema Drawback/DXApplication1/DXApplication1/Frm_Grupos.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+
+namespace DXApplication1
+{
+    public partial class Frm_Grupos : DevExpress.XtraEditors.XtraForm
+    {
+        public Frm_Grupos()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Sistema Drawback/DXApplication1/DXApplication1/Frm_SubGrupos.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+
+namespace DXApplication1
+{
+    public partial class Frm_SubGrupos : DevExpress.XtraEditors.XtraForm
+    {
+        public Frm_SubGrupos()
+        {
+            InitializeComponent();
+        }
+    }
+}
