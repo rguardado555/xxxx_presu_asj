163acafd9df52f4d77f5dcbd4c15b3496e7bc1a1
rguardado555 <rguardado.rsoft@gmail.com>
2018-04-04 10:07:37 -0500

    kardex

---
+++ Sistema Drawback/DXApplication1/DXApplication1/Frm_Kardex.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace DXApplication1
+{
+    partial class Frm_Kardex
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Frm_Kardex));
+            this.monoFlat_Panel1 = new MonoFlat.MonoFlat_Panel();
+            this.monoFlat_Panel2 = new MonoFlat.MonoFlat_Panel();
+            this.monoFlat_HeaderLabel1 = new MonoFlat.MonoFlat_HeaderLabel();
+            this.textEdit1 = new DevExpress.XtraEditors.TextEdit();
+            this.textEdit2 = new DevExpress.XtraEditors.TextEdit();
+            this.monoFlat_HeaderLabel2 = new MonoFlat.MonoFlat_HeaderLabel();
+            this.bunifuFlatButton1 = new Bunifu.Framework.UI.BunifuFlatButton();
+            this.gridControl1 = new DevExpress.XtraGrid.GridControl();
+            this.gridView1 = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.monoFlat_Panel1.SuspendLayout();
+            this.monoFlat_Panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit1.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit2.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // monoFlat_Panel1
+            // 
+            this.monoFlat_Panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.monoFlat_Panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(51)))), ((int)(((byte)(63)))));
+            this.monoFlat_Panel1.Controls.Add(this.bunifuFlatButton1);
+            this.monoFlat_Panel1.Controls.Add(this.monoFlat_HeaderLabel2);
+            this.monoFlat_Panel1.Controls.Add(this.textEdit2);
+            this.monoFlat_Panel1.Controls.Add(this.textEdit1);
+            this.monoFlat_Panel1.Controls.Add(this.monoFlat_HeaderLabel1);
+            this.monoFlat_Panel1.Location = new System.Drawing.Point(3, 1);
+            this.monoFlat_Panel1.Name = "monoFlat_Panel1";
+            this.monoFlat_Panel1.Padding = new System.Windows.Forms.Padding(5);
+            this.monoFlat_Panel1.Size = new System.Drawing.Size(1005, 97);
+            this.monoFlat_Panel1.TabIndex = 0;
+            this.monoFlat_Panel1.Text = "monoFlat_Panel1";
+            // 
+            // monoFlat_Panel2
+            // 
+            this.monoFlat_Panel2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.monoFlat_Panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(51)))), ((int)(((byte)(63)))));
+            this.monoFlat_Panel2.Controls.Add(this.gridControl1);
+            this.monoFlat_Panel2.Location = new System.Drawing.Point(3, 100);
+            this.monoFlat_Panel2.Name = "monoFlat_Panel2";
+            this.monoFlat_Panel2.Padding = new System.Windows.Forms.Padding(5);
+            this.monoFlat_Panel2.Size = new System.Drawing.Size(1004, 393);
+            this.monoFlat_Panel2.TabIndex = 1;
+            this.monoFlat_Panel2.Text = "monoFlat_Panel2";
+            // 
+            // monoFlat_HeaderLabel1
+            // 
+            this.monoFlat_HeaderLabel1.AutoSize = true;
+            this.monoFlat_HeaderLabel1.BackColor = System.Drawing.Color.Transparent;
+            this.monoFlat_HeaderLabel1.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold);
+            this.monoFlat_HeaderLabel1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.monoFlat_HeaderLabel1.Location = new System.Drawing.Point(4, 29);
+            this.monoFlat_HeaderLabel1.Name = "monoFlat_HeaderLabel1";
+            this.monoFlat_HeaderLabel1.Size = new System.Drawing.Size(58, 20);
+            this.monoFlat_HeaderLabel1.TabIndex = 0;
+            this.monoFlat_HeaderLabel1.Text = "Codigo";
+            // 
+            // textEdit1
+            // 
+            this.textEdit1.Location = new System.Drawing.Point(78, 31);
+            this.textEdit1.Name = "textEdit1";
+            this.textEdit1.Size = new System.Drawing.Size(125, 20);
+            this.textEdit1.TabIndex = 1;
+            // 
+            // textEdit2
+            // 
+            this.textEdit2.Location = new System.Drawing.Point(311, 31);
+            this.textEdit2.Name = "textEdit2";
+            this.textEdit2.Size = new System.Drawing.Size(524, 20);
+            this.textEdit2.TabIndex = 2;
+            // 
+            // monoFlat_HeaderLabel2
+            // 
+            this.monoFlat_HeaderLabel2.AutoSize = true;
+            this.monoFlat_HeaderLabel2.BackColor = System.Drawing.Color.Transparent;
+            this.monoFlat_HeaderLabel2.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold);
+            this.monoFlat_HeaderLabel2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.monoFlat_HeaderLabel2.Location = new System.Drawing.Point(206, 29);
+            this.monoFlat_HeaderLabel2.Name = "monoFlat_HeaderLabel2";
+            this.monoFlat_HeaderLabel2.Size = new System.Drawing.Size(90, 20);
+            this.monoFlat_HeaderLabel2.TabIndex = 3;
+            this.monoFlat_HeaderLabel2.Text = "Descripcion";
+            // 
+            // bunifuFlatButton1
+            // 
+            this.bunifuFlatButton1.Activecolor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(139)))), ((int)(((byte)(87)))));
+            this.bunifuFlatButton1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(139)))), ((int)(((byte)(87)))));
+            this.bunifuFlatButton1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.bunifuFlatButton1.BorderRadius = 0;
+            this.bunifuFlatButton1.ButtonText = "Buscar";
+            this.bunifuFlatButton1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.bunifuFlatButton1.DisabledColor = System.Drawing.Color.Gray;
+            this.bunifuFlatButton1.Iconcolor = System.Drawing.Color.Transparent;
+            this.bunifuFlatButton1.Iconimage = ((System.Drawing.Image)(resources.GetObject("bunifuFlatButton1.Iconimage")));
+            this.bunifuFlatButton1.Iconimage_right = null;
+            this.bunifuFlatButton1.Iconimage_right_Selected = null;
+            this.bunifuFlatButton1.Iconimage_Selected = null;
+            this.bunifuFlatButton1.IconMarginLeft = 0;
+            this.bunifuFlatButton1.IconMarginRight = 0;
+            this.bunifuFlatButton1.IconRightVisible = true;
+            this.bunifuFlatButton1.IconRightZoom = 0D;
+            this.bunifuFlatButton1.IconVisible = true;
+            this.bunifuFlatButton1.IconZoom = 90D;
+            this.bunifuFlatButton1.IsTab = false;
+            this.bunifuFlatButton1.Location = new System.Drawing.Point(853, 29);
+            this.bunifuFlatButton1.Name = "bunifuFlatButton1";
+            this.bunifuFlatButton1.Normalcolor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(139)))), ((int)(((byte)(87)))));
+            this.bunifuFlatButton1.OnHovercolor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(129)))), ((int)(((byte)(77)))));
+            this.bunifuFlatButton1.OnHoverTextColor = System.Drawing.Color.White;
+            this.bunifuFlatButton1.selected = false;
+            this.bunifuFlatButton1.Size = new System.Drawing.Size(115, 22);
+            this.bunifuFlatButton1.TabIndex = 5;
+            this.bunifuFlatButton1.Text = "Buscar";
+            this.bunifuFlatButton1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.bunifuFlatButton1.Textcolor = System.Drawing.Color.White;
+            this.bunifuFlatButton1.TextFont = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            // 
+            // gridControl1
+            // 
+            this.gridControl1.Location = new System.Drawing.Point(2, 4);
+            this.gridControl1.MainView = this.gridView1;
+            this.gridControl1.Name = "gridControl1";
+            this.gridControl1.Size = new System.Drawing.Size(1000, 375);
+            this.gridControl1.TabIndex = 0;
+            this.gridControl1.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.gridView1});
+            // 
+            // gridView1
+            // 
+            this.gridView1.GridControl = this.gridControl1;
+            this.gridView1.Name = "gridView1";
+            // 
+            // Frm_Kardex
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1009, 491);
+            this.Controls.Add(this.monoFlat_Panel2);
+            this.Controls.Add(this.monoFlat_Panel1);
+            this.Name = "Frm_Kardex";
+            this.Text = "Frm_Kardex";
+            this.monoFlat_Panel1.ResumeLayout(false);
+            this.monoFlat_Panel1.PerformLayout();
+            this.monoFlat_Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit1.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit2.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private MonoFlat.MonoFlat_Panel monoFlat_Panel1;
+        private Bunifu.Framework.UI.BunifuFlatButton bunifuFlatButton1;
+        private MonoFlat.MonoFlat_HeaderLabel monoFlat_HeaderLabel2;
+        private DevExpress.XtraEditors.TextEdit textEdit2;
+        private DevExpress.XtraEditors.TextEdit textEdit1;
+        private MonoFlat.MonoFlat_HeaderLabel monoFlat_HeaderLabel1;
+        private MonoFlat.MonoFlat_Panel monoFlat_Panel2;
+        private DevExpress.XtraGrid.GridControl gridControl1;
+        private DevExpress.XtraGrid.Views.Grid.GridView gridView1;
+    }
+}
---
+++ Sistema Drawback/DXApplication1/DXApplication1/Frm_Kardex.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+
+namespace DXApplication1
+{
+    public partial class Frm_Kardex : DevExpress.XtraEditors.XtraForm
+    {
+        public Frm_Kardex()
+        {
+            InitializeComponent();
+        }
+    }
+}
